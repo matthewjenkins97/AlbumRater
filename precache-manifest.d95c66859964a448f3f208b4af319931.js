551b5b9cb2f2a44d67948a8fcf50519e0068360c
Matthew Jenkins <matthewjenkins97@gmail.com>
2019-12-26 11:52:03 -0500

    Updates

---
+++ precache-manifest.d95c66859964a448f3f208b4af319931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1118597ea86fced690a8fcf5f1b1b00b",
+    "url": "/AlbumRater/index.html"
+  },
+  {
+    "revision": "372c0724107f71de61b9",
+    "url": "/AlbumRater/static/css/main.ef6877ba.chunk.css"
+  },
+  {
+    "revision": "172251937e84c1b719b0",
+    "url": "/AlbumRater/static/js/2.1ad66654.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AlbumRater/static/js/2.1ad66654.chunk.js.LICENSE"
+  },
+  {
+    "revision": "372c0724107f71de61b9",
+    "url": "/AlbumRater/static/js/main.d845ae09.chunk.js"
+  },
+  {
+    "revision": "882ba878f5eb2d4d23fe",
+    "url": "/AlbumRater/static/js/runtime-main.34b74067.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be3b4a5f2ead6102285e5364b4977f4b.js"
+  "/AlbumRater/precache-manifest.d95c66859964a448f3f208b4af319931.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AlbumRater/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
