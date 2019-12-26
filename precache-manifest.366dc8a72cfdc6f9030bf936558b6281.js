6bbb2f39781fb5efdc6fabb23def79508ba36f80
Matthew Jenkins <matthewjenkins97@gmail.com>
2019-12-26 11:04:32 -0500

    Updates

---
+++ precache-manifest.366dc8a72cfdc6f9030bf936558b6281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96af987364ce147610943584276f8fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29ffaf5da483139e36fe",
+    "url": "/static/css/main.ef6877ba.chunk.css"
+  },
+  {
+    "revision": "cf653425bd71816c4e3c",
+    "url": "/static/js/2.38717c96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.38717c96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29ffaf5da483139e36fe",
+    "url": "/static/js/main.dc357e14.chunk.js"
+  },
+  {
+    "revision": "8e66c5831153fbec2650",
+    "url": "/static/js/runtime-main.8f741a1b.js"
+  }
+]);
