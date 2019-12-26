a23d4278256524c4abeedb51c274ece74982b2b5
Matthew Jenkins <matthewjenkins97@gmail.com>
2019-12-26 11:48:56 -0500

    ok seriously what the fuck

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import AlbumRater from './components/albumrater';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Album Rater</h1>
-      <hr />
-      <p>For each track on the album, type in its length in minutes and seconds, and whether you liked it or not. Your score will be automatically generated below the list.</p>
-      <AlbumRater></AlbumRater>
-      <hr />
-      <p>Made by <a href="http://matthewjenkins97.github.io">Matthew R. Jenkins</a> in 2019. Application uses <a href="https://reactjs.org/">React</a> and CSS from <a href="http://bettermotherfuckingwebsite.com/">bettermotherfuckingwebsite.com</a>.</p>
-    </div>
+    <HashRouter basename='/'>
+      <div>
+        <h1>Album Rater</h1>
+        <hr />
+        <p>For each track on the album, type in its length in minutes and seconds, and whether you liked it or not. Your score will be automatically generated below the list.</p>
+        <AlbumRater></AlbumRater>
+        <hr />
+        <p>Made by <a href="http://matthewjenkins97.github.io">Matthew R. Jenkins</a> in 2019. Application uses <a href="https://reactjs.org/">React</a> and CSS from <a href="http://bettermotherfuckingwebsite.com/">bettermotherfuckingwebsite.com</a>.</p>
+      </div>
+    </HashRouter>
   );
 }
 
