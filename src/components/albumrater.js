c87a924a100b3a49fc4c0e654db45f692c6a0492
Matthew R. Jenkins <matthewjenkins97@gmail.com>
2020-03-15 21:43:44 -0400

    made some modifications to the album rater so that it could be easier to use

--- src/components/albumrater.js
+++ src/components/albumrater.js
@@ -1,115 +1,129 @@
 import React from 'react';
 
 export default class AlbumRater extends React.Component {
+  /**
+   * Represents an Album Rater object. Creates an initial state with 0 tracks.
+   * @constructor
+   * @param {object} props - Props for the AlbumRater object (not used).
+   */
   constructor(props) {
     super(props);
 
     // setting initial variables
     this.trackNumber = 0;
-    this.trackInformation = [];
-    
+
     // binds
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.calculate = this.calculate.bind(this);
   }
 
+  /**
+   * Calculates the score.
+   * @method
+   */
   calculate() {
     // calculates score based on number of seconds of each liked track over number of seconds of every track.
     let liked = 0;
     let total = 0;
 
-    // getting seconds of every liked track, and seconds of every track
-    for (let value of this.trackInformation) {
-      if (value[1]) {
-        liked += value[0];
+    // calculating by every track provided in the <ol>
+    for (let i = 1; i < this.trackNumber + 1; i++) {
+      const minutes = parseInt(document.getElementById('trackMinutes' + i).value);
+      const seconds = parseInt(document.getElementById('trackSeconds' + i).value);
+      const approval = document.getElementById('trackApproval' + i).checked;
+
+      // edge guarding
+      // anything blank?
+      if (isNaN(minutes) || isNaN(seconds)) {
+        alert('Please fill any missing data.');
+        return;
       }
-      total += value[0];
+      // anything not positive?
+      if (minutes < 0 || seconds < 0) {
+        alert('Minutes or seconds cannot be negative numbers.');
+        return;
+      }
+      // seconds greater than 60?
+      if (seconds >= 60) {
+        alert('Number of seconds cannot be greater than 60.');
+        return;
+      }
+
+      // calculation
+      if (approval) {
+        liked += parseInt(minutes) * 60;
+        liked += parseInt(seconds);
+      }
+      total += parseInt(minutes) * 60;
+      total += parseInt(seconds);
     }
 
-    // doing math
+    // presenting the math
     // nothing is displayed if calculated score is NaN
     if (isNaN(Math.round(liked / total))) {
-      document.getElementById("calculatedScore").innerHTML = "";
+      document.getElementById('calculatedScore').innerHTML = '';
     } else {
-      document.getElementById("calculatedScore").innerHTML = `Album rating: ${Math.round((liked / total) * 10) / 2} out of 5`;
+      document.getElementById('calculatedScore').innerHTML = `Album rating: ${Math.round((liked / total) * 10) / 2} out of 5`;
     }
   }
 
+  /**
+   * Adds a track to the ordered list.
+   * @method
+   */
   addTrack() {
-    // variables for adding
-    const minutes = document.getElementById("trackMinutes").value;
-    const seconds = document.getElementById("trackSeconds").value;
-    const approval = document.getElementById("trackApproval").checked;
-
-    // edge guarding
-    if (minutes < 0 || seconds < 0) {
-      alert("Minutes or seconds cannot be negative numbers.")
-      return;
-    }
-    if (seconds >= 60) {
-      alert("Number of seconds cannot be greater than 60.")
-      return;
-    }
-
-    // increment track number (used for html IDs)
     this.trackNumber += 1;
 
     // creating node
-    let node = document.createElement("li");
-    node.id = "track" + this.trackNumber;
-    node.innerHTML = `${minutes.padStart(1, "0")}:${seconds.padStart(2, "0")} - ${approval}`
+    const node = document.createElement('li');
+    node.id = 'track' + this.trackNumber;
+    node.innerHTML = `
+      <input id=trackMinutes${this.trackNumber} type=number></input>
+      <input id=trackSeconds${this.trackNumber} type=number></input>
+      <input id=trackApproval${this.trackNumber} type=checkbox></input>
+    `;
 
     // adding to track information in order to calculate
-    this.trackInformation.push([(parseInt(minutes) * 60) + parseInt(seconds), approval])
-    document.getElementById("trackList").append(node);
-
-    // resetting buttons
-    document.getElementById("trackMinutes").value = "";
-    document.getElementById("trackSeconds").value = "";
-    document.getElementById("trackApproval").checked = false;
+    document.getElementById('trackList').append(node);
 
     // refreshing widgets
-    this.calculate();
     this.forceUpdate();
   }
 
+  /**
+   * Removes the last track from the ordered list.
+   * @method
+   */
   removeTrack() {
     // if track number > 0 you can remove and pop things, otherwise do nothing
     if (this.trackNumber > 0) {
-      let nodeID = "track" + this.trackNumber;
+      const nodeID = 'track' + this.trackNumber;
       document.getElementById(nodeID).remove();
-      this.trackInformation.pop();
     }
 
     // set track number to 0
     this.trackNumber = this.trackNumber > 0 ? this.trackNumber - 1: 0;
 
-    this.calculate();
     this.forceUpdate();
   }
 
+  /**
+   * Renders object.
+   * @method
+   * @return {JSX}
+  */
   render() {
     return (
       <div>
         <ol id="trackList">
         </ol>
-        <div>
-          <label>Minutes: </label>
-          <input id="trackMinutes" type="number"></input>
-        </div>
-        <div>
-          <label>Seconds: </label>
-          <input id="trackSeconds" type="number"></input>
-        </div>
-        <div>
-          <label>Good Track? </label>
-          <input id="trackApproval" type="checkbox"></input>
-        </div>
         <button onClick={this.addTrack}>+</button>
         <button onClick={this.removeTrack}>-</button>
-        <h3 id="calculatedScore"> </h3>
+        <br />
+        <button onClick={this.calculate}>Calculate!</button>
+        <h3 id='calculatedScore'></h3>
       </div>
-    )
+    );
   };
 }
