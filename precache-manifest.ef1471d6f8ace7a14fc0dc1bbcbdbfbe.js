274cac4d490f6f79b1ec9a807a1011d3e31cbbb1
matthewjenkins97 <matthewjenkins97@gmail.com>
2021-10-09 13:19:52 -0400

    Updates

---
+++ precache-manifest.ef1471d6f8ace7a14fc0dc1bbcbdbfbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37430251f6f38e4f9ad9550936961541",
+    "url": "/SpotifyAlbumRater/index.html"
+  },
+  {
+    "revision": "7d5b25c417cc6a97b982",
+    "url": "/SpotifyAlbumRater/static/css/main.ef6877ba.chunk.css"
+  },
+  {
+    "revision": "dcbeb2e9ed6cd0d2d537",
+    "url": "/SpotifyAlbumRater/static/js/2.ecf63f31.chunk.js"
+  },
+  {
+    "revision": "4ff5521e5df92214d8b49925891f4aa3",
+    "url": "/SpotifyAlbumRater/static/js/2.ecf63f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5b25c417cc6a97b982",
+    "url": "/SpotifyAlbumRater/static/js/main.7d92543c.chunk.js"
+  },
+  {
+    "revision": "7e51f6f58dd4afdc1114",
+    "url": "/SpotifyAlbumRater/static/js/runtime-main.ec2812eb.js"
+  }
+]);
