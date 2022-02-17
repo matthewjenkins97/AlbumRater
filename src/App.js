c6ad8c3bccf9b17bd8df615f61a44d49bffe2e33
matthewjenkins97 <matthewjenkins97@gmail.com>
2022-02-16 19:38:37 -0500

    backing up changes to switch VMs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <AlbumRater></AlbumRater>
       <hr />
       <p>Made by <a href="http://matthewjenkins97.github.io">Matthew R. Jenkins</a> in 2021. Application uses <a href="https://reactjs.org/">React</a> and CSS from <a href="http://bettermotherfuckingwebsite.com/">bettermotherfuckingwebsite.com</a>.</p>
-      <p>Note that this is still in development! If you encounter any bugs feel free to report them on my <a href="https://github.com/matthewjenkins97/SpotifyAlbumRater/issues">github page</a>.</p>
+      <p>Note that this is still in development! If you encounter any bugs feel free to report them on my <a href="https://github.com/matthewjenkins97/SpotifyAlbumRater/issues">Github page</a>.</p>
     </div>
   );
 }
